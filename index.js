d1710fb2b38557bc95cb530b01a2ccdf13be67df
Yehia Saleh <yehia.saleh@yale.edu>
2018-01-01 13:33:09 -0500

    Refactor project structure, add client side

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const r = require('rethinkdb');
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
 	res.send({name: 'Yehia'});
 });
 
 // Dynamic port specification for Heroku depolyment
-const PORT = process.env.PORT || 3000;
-app.listen(PORT);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+	console.log(`App is running on ${PORT}`);
+});
