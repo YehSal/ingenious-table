26f9980587460380c2c762797c830228230c5ec3
Yehia Saleh <yehia.saleh@yale.edu>
2018-01-02 11:15:49 -0500

    Added mock table

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,20 +3,21 @@ import {BrowserRouter, Route} from 'react-router-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import Header from './Header';
+import TableExampleControlled from './Table';
 
-
-const Table = () => <h2>Table</h2>;
 
 const App = () => {
 	return (
-		<div>
-			<BrowserRouter>
-				<div> 
-					<Header />
-					<Route exact path="/" component={Table} />
-				</div>
-			</BrowserRouter>
-		</div>
+		<MuiThemeProvider>
+			<div>
+				<BrowserRouter>
+					<div> 
+						<Header />
+						<Route exact path="/" component={TableExampleControlled} />
+					</div>
+				</BrowserRouter>
+			</div>
+		</MuiThemeProvider>
 	);
 };
 
